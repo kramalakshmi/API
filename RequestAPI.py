d92d0b6e0b7fb636864fffceeceef0deec82422b
kramalakshmi <ramalakshmi.kavya@gmail.com>
2026-02-02 12:23:47 -0500

    Update RequestAPI.py

--- RequestAPI.py
+++ RequestAPI.py
@@ -5,4 +5,12 @@ base_url ="https://gorest.co.in"
 #Auth token
 auth_token = "Bearer d5fc969f6b60ddb68552800e3cdf7bf384b2489372ee15c773445b658000f405"
 
-print("Test")
+def get_req():
+  url = base_url+"public/v2/users/"
+  headers = {"Authorization" :auth_token}
+  response = requests.get(url, headers= headers)
+  asert response.status_code ==200
+  json_data = response.json()
+  print("Json response body: ".json_data)
+
+get_req()
