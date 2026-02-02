ac38751511d6a47afc564aab0879b21638ea585c
kramalakshmi <ramalakshmi.kavya@gmail.com>
2026-02-02 15:25:40 -0500

    Update RequestAPI.py

--- RequestAPI.py
+++ RequestAPI.py
@@ -5,46 +5,79 @@ base_url ="https://gorest.co.in"
 #Auth token
 auth_token = "Bearer d5fc969f6b60ddb68552800e3cdf7bf384b2489372ee15c773445b658000f405"
 
-def get_req():
-  url = base_url+"/public/v2/users/"
-  headers = {"Authorization" :auth_token}
-  response = requests.get(url, headers= headers)
-  assert response.status_code ==200
-  json_data = response.json()
-  json_str = json.dumps(json_data, indent=4)
-  print("Json put response body: " , json_str)
+#GET Request
+def get_request():
+    url = base_url + "/public/v2/users"
+    print("get url: " + url)
+    headers = {"Authorization": auth_token}
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print("json GET response body: ", json_str)
+    print("GET USER IS DONE")
+    
 
-def post_req():
-  data ={
-    "name":"Naveen",
-  "email": "naveen@auto.com",
-  "gender":"Male",
-  "status":"Inactive"
-  }
-  url = base_url+f"/public/v2/users"
-  headers = {"Authorization" :auth_token}
-  response = requests.post(url,json=data, headers= headers)
-  assert response.status_code ==201
-  json_data = response.json()
-  json_str = json.dumps(json_data, indent=4)
-  print("Json put response body: " , json_str)
-  
-def put_req(user_id):
-  data ={
-    "name":"Naveen",
-  "email": "naveen@auto.com",
-  "gender":"Male",
-  "status":"active"
-  }
-  url = base_url+f"/public/v2/users/{user_id}"
-  headers = {"Authorization" :auth_token}
-  response = requests.put(url,json=data, headers= headers)
-  assert response.status_code ==200
-  json_data = response.json()
-  json_str = json.dumps(json_data, indent=4)
-  print("Json put response body: " , json_str)
+#POST Request
+def post_request():
+    url = base_url + "/public/v2/users"
+    print("post url: " + url)
+    headers = {"Authorization": auth_token}
+    data = {
+        "name": "Naveen Automation",
+        "email": generate_random_email(),
+        "gender": "male",
+        "status": "active"
+    }
+    response = requests.post(url, json=data, headers=headers)
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print("json POST response body: ", json_str)
+    user_id = json_data["id"]
+    print("user id ===>", user_id)
+    assert response.status_code == 201
+    assert "name" in json_data
+    assert json_data["name"] == "Naveen Automation"
+    print("POST/Create USER IS DONE")
+   
+    return user_id
 
-#post_req()
-get_req()
-put_req(8355242)
-get_req()
+
+#PUT Request
+def put_request(user_id):
+    url = base_url + f"/public/v2/users/{user_id}"
+    print("PUT url: " + url)
+    headers = {"Authorization": auth_token}
+    data = {
+        "name": "Naveen Automation Labs",
+        "email": generate_random_email(),
+        "gender": "male",
+        "status": "inactive"
+    }
+    response = requests.put(url, json=data, headers=headers)
+    assert response.status_code == 200
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print("json PUT response body: ", json_str)
+    assert json_data["id"] == user_id
+    assert json_data["name"] == "Naveen Automation Labs"
+    print("PUT/Update USER IS DONE")
+    
+
+
+#DELETE Request
+def delete_request(user_id):
+    url = base_url + f"/public/v2/users/{user_id}"
+    print("DELETE url: " + url)
+    headers = {"Authorization": auth_token}
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 204
+    print("DELETE USER IS DONE")
+   
+
+
+#call
+get_request()
+user_id = post_request()
+put_request(user_id)
+delete_request(user_id)
