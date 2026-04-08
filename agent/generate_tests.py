5689e0f98445e29e5e642fdba29a275bdd866b70
kramalakshmi <ramalakshmi.kavya@gmail.com>
2026-04-08 14:27:39 -0400

    Create generate_tests.py

---
+++ agent/generate_tests.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+from github import Github
+from github import Auth
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+MODEL = "gpt-5.4"
+TOKEN = os.getenv("PAT")
+auth = Auth.Token(TOKEN)
+
+
+def generate_tests_for_file(file_path):
+    code = Path(file_path).read_text()
+
+    prompt = f"""
+    Generate pytest unit tests for the following Python code.
+    Use clear, deterministic test cases.
+    Return only the Python code. Do not include explanations,Do not include comments and Do not include docstrings
+    I want pytest tests without any extra lines, no chatter, no comments, no unnecessary whitespace, no print statements, no example usage.
+Just clean, minimal test code.
+
+
+    Code:
+    {code}
+    """
+
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "You are an expert Python developer. Write only the code, no explanations ,  comments, or docstrings.
+            I want pytest tests without any extra lines, no chatter, no comments, no unnecessary whitespace, no print statements, no example usage.
+Just clean, minimal test code.
+"},
+        {"role": "user", "content": prompt}
+            
+    )
+
+    return response.choices[0].message.content
+
+
+
+def main():
+    src_dir = Path("src")
+    test_dir = Path("tests")
+    test_dir.mkdir(exist_ok=True)
+
+    for file in src_dir.glob("*.py"):
+        if not "___init__" in str(file): 
+            print(str(Path(file).stem))
+            
+            test_code = generate_tests_for_file(file)
+            test_file = test_dir / f"test_{Path(file).stem}.py"
+            commit_file(str(test_file), test_code)
+            
+
+
+def commit_file(path, content):
+    g = Github(auth=auth)
+    repo = g.get_repo("kramalakshmi/API")
+
+    try:
+        existing = repo.get_contents(path)
+        repo.update_file(
+            path, "Update generated tests", content, existing.sha
+        )
+    except:
+        repo.create_file(
+            path, "Add generated tests", content , branch= "Agents"
+        )
+
+
+
+def write_to_github(path, message, content, branch="main"):
+    try:
+        # Check if the file already exists to update it
+        contents = repo.get_contents(path, ref=branch)
+        repo.update_file(contents.path, message, content, contents.sha, branch=branch)
+        print(f"File '{path}' updated successfully.")
+    except Exception:
+        # If the file doesn't exist, create it
+        repo.create_file(path, message, content, branch=branch)
+        print(f"File '{path}' created successfully.")
+
+
+if __name__ == "__main__":
+    main()
